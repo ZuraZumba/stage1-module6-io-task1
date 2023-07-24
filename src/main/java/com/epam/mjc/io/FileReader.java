7d79ff99116b68f71a299c61dde3263a58539c9f
zurazumba <zura.zumbadze@yahoo.com>
2023-07-25 02:08:18 +0400

    io

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,46 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            String name = null;
+            Integer age = null;
+            String email = null;
+            Long phone = null;
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("Name:")) {
+                    name = line.substring(6).trim();
+                } else if (line.startsWith("Age:")) {
+                    age = Integer.parseInt(line.substring(5).trim());
+                } else if (line.startsWith("Email:")) {
+                    email = line.substring(7).trim();
+                } else if (line.startsWith("Phone:")) {
+                    phone = Long.parseLong(line.substring(7).trim());
+                }
+            }
+
+            return new Profile(name, age, email, phone);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String fileName = "Profile.txt";
+        File file = new File(FileReader.class.getResource("/" + fileName).getFile());
+        FileReader fileReader = new FileReader();
+
+        Profile profile = fileReader.getDataFromFile(file);
+        System.out.println("Profile data:");
+        System.out.println("Name: " + profile.getName());
+        System.out.println("Age: " + profile.getAge());
+        System.out.println("Email: " + profile.getEmail());
+        System.out.println("Phone: " + profile.getPhone());
     }
 }
